2c6dcaa2fd0334c2206ee2ce89e840c4c6016849
Gabor Kevin Barta <gkevinba@gmail.com>
2021-09-18 22:22:18 +0200

    Make adjustments to android.py

--- android.py
+++ android.py
@@ -1,20 +1,24 @@
 from turtle import *
 
-speed(1)
+speed(100)
 bgcolor("black")
 pen(pencolor="#80b602")
 shape("square")
 fillcolor("#80b602")
 penup()
 width(1)
-shapesize(6, 6, 40)
-# shapesize(5, 6, 40)
+seth(90)
+forward(30)
+shapesize(6, 5, 40)
+
 stamp()
+
+shapesize(6, 6, 40)
 
 
 shape("circle")
-seth(90)
-forward(65)
+# seth(90)
+forward(55)
 stamp()
 
 hideturtle()
@@ -23,17 +27,17 @@ def limb(x, y):
     goto(x, y)
     pendown()
     width(40)
-    forward(80)
+    forward(70)
     penup()
 
-limb(110, -30)
-limb(-110, -30)
-limb(33, -120)
-limb(-33, -120)
+limb(108, 0)
+limb(-108, 0)
+limb(33, -80)
+limb(-33, -80)
 
-goto(35, 106)
+goto(35, 128)
 dot(14, "black")
-goto(-35, 106)
+goto(-35, 128)
 dot(14, "black")
 
 def antenna(x, y, angle):
@@ -44,12 +48,12 @@ def antenna(x, y, angle):
     forward(35)
     penup()
 
-antenna(35, 130, 65)
-antenna(-35, 130, 115)
+antenna(30, 150, 65)
+antenna(-30, 150, 115)
 
 penup()
 
-goto(80, 70)
+goto(80, 90)
 pen(pencolor="black")
 width(8)
 pendown()
