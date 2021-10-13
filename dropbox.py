28c00edf651000bb5ad2e19aa9fc72b1c8a5f4b1
Gabor Kevin Barta <gkevinba@gmail.com>
2021-10-13 23:34:54 +0200

    Clean up dropbox.py

--- dropbox.py
+++ dropbox.py
@@ -6,7 +6,7 @@ def diamond(x, y):
     seth(180)
     forward(53)
     stamp()
-speed(100)
+speed(1)
 bgcolor("white")
 pen(pencolor="#0060FF")
 shape("triangle")
@@ -15,38 +15,8 @@ penup()
 width(1)
 shapesize(5, 4.5, 1)
 diamond(20, 6)
-diamond(108, 70)
-diamond(-68, 70)
-diamond(108, 172)
-diamond(-68, 172)
-done()
-
-# from turtle import *
-
-# def diamond(x, y):
-#     seth(0)
-#     goto(x, y)
-#     stamp()
-#     seth(180)
-#     forward(47)
-#     stamp()
-
-# speed(100)
-# bgcolor("#0060FF")
-# pen(pencolor="white")
-# shape("triangle")
-# fillcolor("white")
-# penup()
-# width(1)
-# shapesize(5, 4, 1)
-
-
-# diamond(20, 6)
-# diamond(90, 70)
-# diamond(-50, 70)
-# diamond(90, 172)
-# diamond(-50, 172)
-
-
-
+diamond(100, 70)
+diamond(-60, 70)
+diamond(100, 172)
+diamond(-60, 172)
 done()
